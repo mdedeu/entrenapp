549be4f9931e037239f1e82d246be8524174e7d6
Marcos Dedeu <mdedeu@itba.edu.ar>
2021-09-16 21:17:08 -0300

    Headers and SASS variables

---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app-bar
+      outlined
+      app
+      color = "primary"
+    class="">
+    <v-img :src="require('../assets/logo.png')"
+           alt="Vuetify Logo"
+           class="shrink mr-2"
+           contain
+           transition="scale-transition"
+           width="40">
+    </v-img>
+    <div class="d-inline-flex ml-16 col-4">
+      <h4 class="white--text  mr-6">Creá tu rutina</h4>
+      <h4 class="white--text mr-6">Rutinas</h4>
+      <h4 class="white--text mr-6">Ejercicios</h4>
+      <h4 class="white--text mr-6">Mi perfil</h4>
+    </div>
+
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "HeaderApp"
+}
+</script>
+
+<style scoped>
+
+</style>
