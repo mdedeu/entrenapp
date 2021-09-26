fc5f090dc95b6c3cbf3e1b23ba5139e660084937
Marcos Dedeu <mdedeu@itba.edu.ar>
2021-09-25 21:44:59 -0300

    add Ruteo

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,5 @@
 <template>
-  <v-app>
-    <HeaderHome></HeaderHome>
-    <v-main class="primary">
-      <Home></Home>
-    </v-main>
-  </v-app>
+    <v-app>
+      <router-view/>
+    </v-app>
 </template>
-
-<script>
-import HeaderHome from "./components/HeaderHome";
-import Home from "./components/Home";
-
-export default {
-  name: 'App',
-
-  components: {
-    HeaderHome,
-    Home,
-  },
-
-  data: () => ({
-    //
-  }),
-};
-</script>
