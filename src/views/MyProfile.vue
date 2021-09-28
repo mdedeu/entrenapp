6249288aa84a40dafaefd1de88eb5cb5e4a72dad
gbeade <67883264+gbeade@users.noreply.github.com>
2021-09-28 16:20:46 -0300

    Initial profile commit

---
+++ src/views/MyProfile.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <HeaderApp></HeaderApp>
+  </div>
+</template>
+
+<script>
+import HeaderApp from "../components/HeaderApp";
+
+export default {
+  name: "MyProfile.vue",
+  components: {
+    HeaderApp
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
