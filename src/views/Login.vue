a383a1cc363f234c52d58446864eb8a6822be4a5
Marcos Dedeu <mdedeu@itba.edu.ar>
2021-09-27 22:16:06 -0300

    Login Layout OK

---
+++ src/views/Login.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-container fluid  fill-height class="primary">
+      <v-row class="mt-16">
+        <v-col></v-col>
+        <v-col >
+          <router-link :to= "{name: 'Home'}">
+            <v-img src = "../assets/logo.png" class="mr-auto ml-auto" alt="logo" width="50%"/>
+          </router-link>
+       </v-col>
+        <v-col></v-col>
+      </v-row>
+      <v-row>
+        <v-col></v-col>
+        <v-col>  <v-text-field   label="Usuario"
+                                 required
+                                 solo
+        ></v-text-field> </v-col>
+        <v-col></v-col>
+      </v-row>
+      <v-row>
+        <v-col></v-col>
+        <v-col>  <v-text-field label="Contraseña"
+                               required
+                               solo
+        ></v-text-field> </v-col>
+        <v-col></v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+        </v-col>
+        <v-col class="text-center">
+          <v-btn class="mt-3">Ingresá</v-btn>
+        </v-col>
+        <v-col></v-col>
+      </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Login.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
