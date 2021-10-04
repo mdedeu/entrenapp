766c19aec1829bb75912b4e42f542878217930e1
sal <salCas276@gmail.com>
2021-10-03 22:38:05 -0300

    Perimera version de tus_rutinas salva

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import TusRutinas from "../views/TusRutinas";
 
 Vue.use(VueRouter)
 
@@ -9,7 +10,12 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
-   }
+   },
+  {
+    path: '/TusRutinas',
+    name: 'TusRutinas',
+    component: TusRutinas
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/TusRutinas.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-container fluid class="primary fill-height mt=16" height="300px">
+    <v-row style="height: 50px">
+    <HeaderApp></HeaderApp>
+    </v-row>
+      <v-row  align="center" justify="center">
+        <v-col cols="3" align="center" justify="center">
+          <h1 class="mb7 accent--text">Tus Rutinas</h1>
+        </v-col>
+      </v-row>
+    <v-row align="center" justify="center" >
+      <v-col align="center" justify="center" cols="3">
+        <v-text-field
+            -icon="mdi-magnify"
+          class="white"
+          label="Nombre"
+          rounded
+          height="10px"
+      ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row style="height: 10px"></v-row>
+    <v-row>
+    </v-row>
+    <v-row align="center" justify="center" >
+      <v-col align="center" justify="center">
+        <v-btn class="black--text" rounded color="white" width="800" height="60">
+          Rounded Button
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+    </v-row>
+    <v-row align="center" justify="center" >
+      <v-col align="center" justify="center">
+        <v-btn class="black--text" rounded color="white" width="800" height="60">
+          Rounded Button
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+    </v-row>
+    <v-row align="center" justify="center" >
+      <v-col align="center" justify="center">
+        <v-btn class="black--text" rounded color="white" width="800" height="60">
+          Rounded Button
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+    </v-row>
+    <v-row align="center" justify="center" >
+      <v-col align="center" justify="center">
+        <v-btn class="black--text" rounded color="white" width="800" height="60">
+          Rounded Button
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row style="height: 30px">
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import HeaderApp from "../components/HeaderApp";
+
+export default {
+  name: 'TusRutinas',
+  components: {
+    HeaderApp,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
