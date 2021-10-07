3bc362398f330481a6d4a79d17a8c3f2219a692a
shadad00 <shadad@itba.edu.ar>
2021-10-07 08:51:30 -0300

    Se agrego el template para comunicacion con la API dado por la catedra.

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import module1 from './modules/module1'
+import security from '@/store/module/security';
+import sport from '@/store/module/sport';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    modules:{
-        module1
-    },
-    email : null
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+    security,
+    sport
+  }
 })
