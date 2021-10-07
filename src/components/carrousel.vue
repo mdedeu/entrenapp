da218701e2e4f37628307c0653fa246c22e5606e
gbeade <67883264+gbeade@users.noreply.github.com>
2021-10-07 15:56:30 -0300

    v-group implemented in carrousel

--- src/components/carrousel.vue
+++ src/components/carrousel.vue
@@ -1,32 +1,29 @@
 <template>
     <v-container fluid class="accent fill-height">
-      <v-row justify="center">
-        <v-col cols="3">
+      <v-row justify="center" class="mb-5 mt-4">
+        <v-col cols="5">
           <router-link  :to="{name:`${Section.nameComponent}`}" class="text-h4">{{ Section.title }}</router-link>
         </v-col>
         </v-row  >
-        <v-row>
-          <v-carousel v-model="model" style="height: 30vh">
-            <v-carousel-item  >
-              <template>
-                <v-row>
-                  <v-col v-for="routine in routines" :key="routine.id">
-                    <v-card
-                        className="mx-auto"
-                        max-width="344"
-                        @click="RoutineDescription"
-                    >
-                      <v-card-text>
-                        <div>{{routine.name}}</div>
-                        <p class="text-h4 text--primary">{{routine.name}}</p>
-                        <div class="text--primary">Nivel de dificultad : {{routine.level}}<br></div>
-                      </v-card-text>
-                    </v-card>
-                  </v-col>
-                </v-row>
-              </template>
-            </v-carousel-item>
-          </v-carousel>
+        <v-row class="mb-5 mt-4">
+          <v-slide-group show-arrows >
+            <v-slide-item
+                v-for="routine in routines"
+                :key="routine.id"
+            >
+              <v-card
+                  width="50vh"
+                  class="mx-12"
+                  @click="RoutineDescription"
+              >
+                <v-card-text>
+                  <div>{{routine.name}}</div>
+                  <p class="text-h4 text--primary">{{routine.name}}</p>
+                  <div class="text--primary">Nivel de dificultad : {{routine.level}}<br></div>
+                </v-card-text>
+              </v-card>
+            </v-slide-item>
+          </v-slide-group>
         </v-row>
     </v-container>
 </template>
@@ -36,7 +33,7 @@
     name: "carrousel",
     data(){
       return {
-        routines : [ {id:0,name:"Pecho",level:"1"} , {id:1,name:"Pecho",level:"1"} , {id:2, name:"Pecho",level:"1"}, {id:3, name:"Pecho",level:"1"} ],
+        routines : [ {id:0,name:"Pecho",level:"1"} , {id:1,name:"Pecho",level:"1"} , {id:2, name:"Pecho",level:"1"}, {id:3, name:"Pecho",level:"1"}, {id:3, name:"Pecho",level:"1"}, {id:3, name:"Pecho",level:"1"}, {id:3, name:"Pecho",level:"1"}, {id:3, name:"Pecho",level:"1"}, {id:3, name:"Pecho",level:"1"}, {id:3, name:"Pecho",level:"1"} ],
       }
   },
     props:{
