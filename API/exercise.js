9b7ec0b6d98bcaf3b401a5b1785ec509490ee0ba
salvaCasta <salCas276@gmail.com>
2021-10-08 15:50:41 -0300

    Agrego contrato exercise para la api

---
+++ API/exercise.js
@@ -0,0 +1,42 @@
+import { Api } from './api.js'
+
+export { ExerciseApi, Exercise }
+
+class ExerciseApi {
+    static getUrl(slug) {
+        return `${Api.baseUrl}/exercises${ slug ? `/${slug}` : ''}`
+    }
+
+    static async add(exercise, controller) {
+        return await Api.post(ExerciseApi.getUrl(), true, exercise, controller)
+    }
+
+    static async modify(exercise, controller) {
+        return await Api.put(ExerciseApi.getUrl(exercise.id), true, exercise, controller)
+    }
+
+    static async delete(id, controller) {
+        return await Api.delete(ExerciseApi.getUrl(id), true, controller)
+    }
+
+    static async get(id, controller) {
+        return await Api.get(ExerciseApi.getUrl(id),true,  controller)
+    }
+
+    static async getAll(controller) {
+        return await Api.get(ExerciseApi.getUrl(), true, controller)
+    }
+}
+
+class Exercise {
+    constructor(id, name, detail, type, date) {
+        if (id) {
+            this.id = id;
+        }
+        this.type=type
+        this.date=date
+        this.name = name;
+        this.detail = detail;
+
+    }
+}
