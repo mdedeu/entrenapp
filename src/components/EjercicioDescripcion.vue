ef6ad66e281e069ca0193b5ff0c3d5aa0a052769
Marcos Dedeu <mdedeu@itba.edu.ar>
2021-10-08 23:44:40 -0300

    Descripcion

--- src/components/EjercicioDescripcion.vue
+++ src/components/EjercicioDescripcion.vue
@@ -1,20 +1,20 @@
 <template>
   <v-container fluid class="primary fill-height" style="height: 100vh">
-    <v-row justify="left">
+    <v-row justify="center">
       <v-col cols="3" >
-        <p class="text-h2 accent--text">{{ejercicio.name}}</p>
+        <p class="text-h4 accent--text">{{ejercicio.name}}</p>
       </v-col>
     </v-row>
 
     <v-row style="height: 50%">
-      <v-container fluid class="white">
-        <p class="text-h3"> Video de la tecnica </p>
+      <v-container fluid>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/QVvY6liKWVg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </v-container>
     </v-row>
 
-    <v-row justify="left">
+    <v-row justify="center">
       <v-col cols="6">
-        <p class="text-h4 accent--text">Musculos que trabaja: {{ejercicio.muscles}}</p>
+        <p class="text-h5 accent--text">Músculos que trabaja: {{ejercicio.muscles}}</p>
       </v-col>
     </v-row>
   </v-container>
