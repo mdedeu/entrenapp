b99d314e2f79dd291e8c1f8782871a135072686c
Marcos Dedeu <mdedeu@itba.edu.ar>
2021-10-09 13:01:30 -0300

    popup descanso

---
+++ src/components/PopupDescanso.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container  class="primary" width="200px">
+    <v-row>
+      <h3 class="accent--text  ma-3">Duración del Descanso en segundos</h3>
+    </v-row>
+    <v-row>
+      <v-text-field
+          label="Duracion"
+          color="accent"
+          class="accent--text mr-12 ml-12"
+          solo
+          clearable
+      ></v-text-field>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-btn
+        rounded color=" accent primary--text">Guardar
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "PopupDescanso"
+}
+</script>
+
+<style scoped>
+
+</style>
